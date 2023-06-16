6c3dae193e4fbca28a8b84572875545007289b66
Kobregon1 <105620454+Kobregon1@users.noreply.github.com>
2023-06-16 01:25:57 -0700

    Update Task_manager.c

--- Task_manager.c
+++ Task_manager.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 #include <time.h>
 
+void Drawing();
+
 int main() {
-    printf("\n\t=============================\t _______________\n");
-    printf("\t|| Welcome To Task Manager ||\t|~~~~~~~~~~~ ||\t|\n");
-    printf("\t=============================\t|~~~~~~~~~~~ ||\t|\n");
-    printf("\t\t\t\t\t|~~~~~~~~~~~ ||\t|\n");
-    printf("\t\t\t\t\t|~~~~~~~~~~~ ||\t|\n");
-    printf("\t\t\t\t\t|~~~~~~~~~~~ ||\t|\n");
-    printf("\t\t\t\t\t|_______________|\n");
-    
+    Drawing();
     
     time_t currentTime;
     struct tm *localTime;
@@ -26,11 +21,21 @@ int main() {
     FILE * fpointer = fopen("Task_Manager.txt", "a");
     char taskName1 [20];
     char taskName2 [20];
-    // Add more task names if needed
+    
     printf("What task did you complete?\n");
     scanf("%s %s", taskName1, taskName2);
     fprintf(fpointer,"%s %s @ %s" , taskName1, taskName2, dateString);
     
     fclose(fpointer);
     return 0;
+}
+
+void Drawing(){
+    printf("\n\t=============================\t _______________\n");
+    printf("\t|| Welcome To Task Manager ||\t|~~~~~~~~~~~ ||\t|\n");
+    printf("\t=============================\t|~~~~~~~~~~~ ||\t|\n");
+    printf("\t\t\t\t\t|~~~~~~~~~~~ ||\t|\n");
+    printf("\t\t\t\t\t|~~~~~~~~~~~ ||\t|\n");
+    printf("\t\t\t\t\t|~~~~~~~~~~~ ||\t|\n");
+    printf("\t\t\t\t\t|_______________|\n");
 }
