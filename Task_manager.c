64a4a12bcedba0e99c97082ab54ac0c42e521000
Kobregon1 <105620454+Kobregon1@users.noreply.github.com>
2023-06-16 01:17:11 -0700

    Add files via upload
    This is a task manager project that will take the input of a task & output to a file called Task_manager.txt with current date & time stamp.

---
+++ Task_manager.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    printf("\n\t=============================\t _______________\n");
+    printf("\t|| Welcome To Task Manager ||\t|~~~~~~~~~~~ ||\t|\n");
+    printf("\t=============================\t|~~~~~~~~~~~ ||\t|\n");
+    printf("\t\t\t\t\t|~~~~~~~~~~~ ||\t|\n");
+    printf("\t\t\t\t\t|~~~~~~~~~~~ ||\t|\n");
+    printf("\t\t\t\t\t|~~~~~~~~~~~ ||\t|\n");
+    printf("\t\t\t\t\t|_______________|\n");
+    
+    
+    time_t currentTime;
+    struct tm *localTime;
+
+    time(&currentTime);
+    localTime = localtime(&currentTime);
+
+    // Convert the current time to a string representation
+    char *dateString = ctime(&currentTime);
+
+    printf("\nCurrent Date & time: %s", dateString);
+    printf("---------------------------------------------\n");
+
+    FILE * fpointer = fopen("Task_Manager.txt", "a");
+    char taskName1 [20];
+    char taskName2 [20];
+    
+    printf("What task did you complete?\n");
+    scanf("%s %s", taskName1, taskName2);
+    fprintf(fpointer,"%s %s @ %s" , taskName1, taskName2, dateString);
+    
+    fclose(fpointer);
+    return 0;
+}
